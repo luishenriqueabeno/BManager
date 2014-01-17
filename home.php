da78e8b87e9280d8810bdebbf5c5a1abe57a3f67
Luis Henrique Abeno <luis_abeno@hotmail.com>
2014-01-17 15:41:18 -0200

    Performance changes

--- home.php
+++ home.php
@@ -25,7 +25,7 @@
 		<!-- Scripts -->
 		<script src = "lib/jquery-1.10.2/jquery-1.10.2.min.js" type = "text/javascript"></script>
 		<script src="resources/js/home.js"></script>
-		<script src="lib/jquery-ui-1.10.3/ui/jquery-ui.js"></script>
+		<script src="lib/jquery-ui-1.10.3/ui/jquery-ui.min.js"></script>
 
 	</head>
 	<body>
