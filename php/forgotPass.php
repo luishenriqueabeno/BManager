11af5497bc40c9292ebb7dd170e81cc454d7ee1e
Luis Henrique Abeno <luis_abeno@hotmail.com>
2014-02-12 21:24:58 -0200

    Forgot pass fixes

--- php/forgotPass.php
+++ php/forgotPass.php
@@ -17,14 +17,17 @@
 
 		$sql = mysql_query($query);
 
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
 		$message .= "Este email contém uma nova senha para acesso ao sistema XYZ. <br>";
-		$message .= "Você poderá alterar a senha e selecionando o item 'Alterar senha', no perfil do usuário<br>";
+		$message .= "Você poderá alterar a senha selecionando o item 'Alterar senha', no perfil do usuário<br>";
 		$message .= "Esta é a sua nova senha: ". $newPass; 
 		$message .= "<br>";
 		$message .= "Atenciosamente, <br><br>";
 		$message .= "Equipe XYZ";
 		
-		$enviado = mail($email, 'Nova senha', $message);
+		$enviado = mail($email, 'Nova senha', $message, $headers);
 
 		// Exibe uma mensagem de resultado
 		if ($enviado) {
