d40bb5d485b405f431c01b0d9a62358da5ebadf1
Luis Henrique Abeno <luis_abeno@hotmail.com>
2014-02-18 14:02:51 -0300

    Tasks min js

--- modules/MyTasks/resources/js/tasks.js
+++ modules/MyTasks/resources/js/tasks.js
@@ -55,15 +55,8 @@ $(document).ready(function(){
 
 	//Para qualquer requisição ajax é adicionado um loader na página
 	$( document ).ajaxStart(function() {
-		var onEdit = true;
-
-		if(onEdit == false){
-			//Exibe o loader até que a requisição seja concluída
-			$('.loader').show();
-		} else {
-			//Exibe o loader até que a requisição seja concluída
-			$('.loader').hide();
-		}
+		//Exibe o loader até que a requisição seja concluída
+		$('.loader').hide();
 	});
 	$( document ).ajaxStop(function() {
 		//Ao terminar uma requisição o loader é escondido
