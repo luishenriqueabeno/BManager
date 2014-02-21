47868a0cf9d6924ed28a7edbdf54f0d7aa54d7fb
Luis Henrique Abeno <luis_abeno@hotmail.com>
2014-02-21 09:57:59 -0300

    Fix base url

--- modules/CashFlow/php/loadExpenses.php
+++ modules/CashFlow/php/loadExpenses.php
@@ -4,6 +4,13 @@
 	//Pega url base
 	$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/';
 
+	//Verifica se é ambiente de produção ou desenvolvimento
+	if($baseUrl == 'http://localhost/'){
+		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/';
+	} else {
+		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/trabalhos/2014/Projeto%20-%20Daily%20Helper/';
+	}
+
 	//Suprime warnings
 	error_reporting(E_ERROR | E_PARSE);
 
