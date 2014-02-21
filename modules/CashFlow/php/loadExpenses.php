b7a41f37832b160e5e48c472acb8c12b40bec3be
Luis Henrique Abeno <luis_abeno@hotmail.com>
2014-02-21 10:00:35 -0300

    Fix base url

--- modules/CashFlow/php/loadExpenses.php
+++ modules/CashFlow/php/loadExpenses.php
@@ -8,7 +8,7 @@
 	if($baseUrl == 'http://localhost/'){
 		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/';
 	} else {
-		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/trabalhos/2014/Projeto%20-%20Daily%20Helper/DailyHelper/';
+		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/trabalhos/2014/Projeto%20-%20Daily%20Helper/';
 	}
 
 	//Suprime warnings
