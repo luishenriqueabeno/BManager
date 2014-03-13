ab3a1b964399e2d1d69163bb7b6d1648c6921304
Luis Henrique Abeno <luis_abeno@hotmail.com>
2014-03-13 16:07:30 -0300

    Some changes

--- home.php
+++ home.php
@@ -1,16 +1,7 @@
 <?php
 	require('php/conn.php');
 	require("secure.php");
-
-	//Pega url base
-	$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/';
-
-	//Verifica se é ambiente de produção ou desenvolvimento
-	if($baseUrl == 'http://localhost/'){
-		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/BManager/';
-	} else {
-		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/trabalhos/2014/BManager/';
-	}
+	require("php/conf.php");
 	
 	$username = $_SESSION['username'];
 
---
+++ php/conf.php
@@ -0,0 +1,11 @@
+<?php
+	//Pega url base
+	$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/';
+
+	//Verifica se é ambiente de produção ou desenvolvimento
+	if($baseUrl == 'http://localhost/'){
+		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/BManager/';
+	} else {
+		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/trabalhos/2014/BManager/';
+	}
+?>
