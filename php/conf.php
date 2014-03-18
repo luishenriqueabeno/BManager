5f6ad499d75d5b34247274f5c755301da2bde425
Luis Henrique Abeno <luis_abeno@hotmail.com>
2014-03-17 22:19:40 -0300

    Image fix

--- php/conf.php
+++ php/conf.php
@@ -6,6 +6,6 @@
 	if($baseUrl == 'http://localhost/'){
 		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/BManager/';
 	} else {
-		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/trabalhos/2014/BManager/';
+		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/trabalhos/BManager/';
 	}
 ?>
