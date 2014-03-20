67ad5999b5f69795b232430f6c202703663bfef3
Luis Henrique Abeno <luis_abeno@hotmail.com>
2014-03-20 16:42:51 -0300

    Logout fix

--- php/logout.php
+++ php/logout.php
@@ -1,16 +1,8 @@
 <?php
+	require("php/conf.php");
+
 	session_start();
 	session_destroy();
-
-	//Pega url base
-	$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/';
-
-	//Verifica se é ambiente de produção ou desenvolvimento
-	if($baseUrl == 'http://localhost/'){
-		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/BManager/';
-	} else {
-		$baseUrl = "http://" . $_SERVER['SERVER_NAME'] . '/trabalhos/2014/BManager/';
-	}
 
 	//Redireciona para a página incial do website
 	header('location: '. $baseUrl);
